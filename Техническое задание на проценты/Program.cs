1c33ac744cb0b241448878088e5006c143b32dff
Raziat <114012022+RaziatOmarova@users.noreply.github.com>
2022-10-12 23:39:28 +0300

    first commit

---
+++ Техническое задание на проценты/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Техническое_задание_на_проценты
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число:");
+            double number = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите процент:");
+            double percent = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Рeзультат:" + Percent(number, percent));
+            Console.ReadKey();
+        }
+        static public double Percent(double numberone, double numbertwo)
+        {
+            return (numberone * numbertwo) / 100;
+        }
+    }
+}
